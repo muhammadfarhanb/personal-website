9ca8b759f23bf9ebe1728a8b721739b6c27162af
farhanbashir <farhan2526313@gmail.com>
2023-06-06 22:58:13 +0500

    mobile responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,11 +13,11 @@ import Contact from "./Contact/page";
 export default function Home() {
   return (
       <div className="bg-white h-screen ">
-          <div className="container mx-auto flex items-center justify-center py-28 px-10 ">
-            <div className=" flex flex-col ">
-                <h1 className="text-blue-600 text-6xl pr-40 font-bold"> I'm Muhammad Farhan
+          <div className="container ">
+            <div className="flex flex-col items-center m-10 ">
+                <h1 className="text-2xl font-bold text-blue-700 lg:text-5xl "> I'm Muhammad Farhan
                   <br className="" />
-                  <div className= "text-4xl text-black">  <Typewriter
+                  <div className= "text-xl font-bold text-black underline lg:text-4xl">  <Typewriter
                         options={{
                         strings: ['Full-Stack Developer',
                          'Front-end Developer',
@@ -26,28 +26,27 @@ export default function Home() {
                         loop: true,
                         }}
                      />
-                     </div>
-
-                </h1>
-                <br/>
-                <div className="w-[100px] h-[4px] bg-black -m-5 mx-1"></div>
-                <p className="text-blue-400 my-10 w-1/2 ">
+                   </div>
+                  </h1>
+                  
+            
+            </div>
+            <div className="flex flex-col items-center ">
+              <div className="m-10">
+                  <Image src={"/image/personal1.jpg"} alt="personal pic" width={250} height={100} 
+                  className="rounded-full shadow-black shadow-2xl " />
+              </div>
+              <div className="flex flex-col items-center flex-wrap pb-12">
+                <p className="text-blue-400 m-7 w-1/2 ">
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
                 when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
                 
                 </p>
-                <Button className="bg-green-500 w-fit  ">Learn more</Button>
-                
+                  <Button className="bg-green-500 w-fit  ">Learn more</Button>
+              </div>
+              </div>
             </div>
-            <div className="">
-                  <Image src={"/image/personal1.jpg"} alt="personal pic" width={500} height={500} 
-                  className="rounded-full shadow-black shadow-2xl " />
-            </div>
-            <div>
-            
-            </div>
-          </div>
           <About/>
           <Skill/>
           <Contact/>
--- src/component/Nav/page.tsx
+++ src/component/Nav/page.tsx
@@ -3,18 +3,18 @@ import Image from "next/image";
 const Nav =() =>{
     return(
 
-        <div className="">        
-            <div className="">
-                <nav className="bg-green-600  flex">
-                    <div className="flex justify-start items-center mx-28 sm:flex-shrink">
+        <div className="bg-blue-600 ">       
+            <div className="container ">
+                <nav className="flex items-center flex-wrap ">
+                    <div className="flex-1">
                        <Image src={"/image/logo.jpg"} alt="logo" width={500} height={300} 
                        className="w-48"/>
                     </div>
-                    <div className="mx-64 p-8 sm:flex-shrink">
-                        <Link className="text-white p-6 font-bold text-l " href={"/"}>Home</Link>
-                        <Link className="text-white p-6 font-bold text-l " href={"About"}>About</Link>
-                        <Link className="text-white p-6 font-bold text-l " href={"Skill"}>Skill</Link>
-                        <Link className="text-white p-6 font-bold text-l " href={"Contact"}>contact</Link>
+                    <div className="flex flex-wrap">
+                        <Link className="text-white p-8 font-bold text-l " href={"/"}>Home</Link>
+                        <Link className="text-white p-8 font-bold text-l " href={"About"}>About</Link>
+                        <Link className="text-white p-8 font-bold text-l " href={"Skill"}>Skill</Link>
+                        <Link className="text-white p-8 font-bold text-l " href={"Contact"}>contact</Link>
                     </div>
                 </nav>
 
